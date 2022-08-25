e7d5d68a1cedccd76ad36c9c17a789bdba6efb22
sanbharadwaj <sanchit.bharadwaj@outlook.com>
2022-08-25 13:09:24 +0530

    img update

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -7,16 +7,15 @@ import {VscFolderLibrary} from 'react-icons/vsc'
 
 const About = () => {
   return (
-    <section id='about'>
-      <h5>Get to Know</h5>
-      <h2>About Me</h2>
 
+      <section id='about'>
       <div className='container about__container'>
         <div className='about__me'>
           <div className='about__me-image'>
             <img src={ME} alt="About" />
           </div>
         </div>
+
 
         <div className='about__content'>
           <div className='about__cards'>
@@ -42,11 +41,12 @@ const About = () => {
           <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Non deserunt voluptas dolorum omnis veritatis aliquid adipisci nam harum, distinctio vel vero. Quisquam corrupti illum, officia omnis mollitia dicta iste nam.
           </p>
 
-          <a href="#contact" className='btn btn-primary'>Let's Talk</a>
+          
   
         </div>
       </div>
     </section>
+
   )
 }
 
